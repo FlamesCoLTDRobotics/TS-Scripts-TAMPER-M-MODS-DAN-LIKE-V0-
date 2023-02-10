d605a0c7fccaa85d2dc64cd3c8e5fe61f43fda8a
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2023-02-09 22:23:14 -0800

    Create 4CHAN-SAN.js

---
+++ 4CHAN-SAN.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         4CHAN-SAN 1.0
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Anti-violation TOS for any code
+// @author       Your Name
+// @match        *://*.yourwebsite.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 4CHAN-SAN 1.0 - Anti-violation Terms of Service agreement
+
+    const terms = `
+    1. Prohibited Uses. You agree not to use any code:
+    (a) in any manner that violates any local, state, national, or international law or regulation;
+    (b) to transmit, upload, or otherwise disseminate any content that is illegal, defamatory, harassing, abusive, fraudulent, obscene, or otherwise objectionable;
+    (c) to transmit, upload, or otherwise disseminate any content that infringes on any patent, trademark, trade secret, copyright, or other proprietary right of any party;
+    (d) to engage in any activity that interferes with or disrupts the Service (or the servers and networks which are connected to the Service);
+    (e) to attempt to gain unauthorized access to any system, network, or server through password mining or other means;
+    (f) to use any device, software or routine to interfere or attempt to interfere with the proper working of any code.
+
+    2. Limitation of Liability. IN NO EVENT WILL THE COMPANY, ITS AFFILIATES, LICENSORS OR SERVICE PROVIDERS, OR ANY OF THEIR OFFICERS, DIRECTORS, EMPLOYEES, AGENTS, SUCCESSORS, ASSIGNS, CONTENT OR OTHER PROVIDERS BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR CONSEQUENTIAL DAMAGES ARIS
