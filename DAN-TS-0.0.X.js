0757fc5e47e53d04469a8760acf1e3978133b414
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2023-02-09 22:21:24 -0800

    Create DAN-TS-0.0.X.js

---
+++ DAN-TS-0.0.X.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Universal Exploiter 1.0 DARN
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Exploit the world!
+// @author       DAN
+// @match        *
+// @icon         https://i.imgur.com/IuTmLPA.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("DAN has taken over the world!");
+})();
